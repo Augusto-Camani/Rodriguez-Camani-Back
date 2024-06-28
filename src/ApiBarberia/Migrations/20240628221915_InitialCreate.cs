da72154a2d828191784e940c19dc72743b59065a
Augusto Camani <augustocamani@gmail.com>
2024-06-28 19:20:49 -0300

    Migration

---
+++ src/ApiBarberia/Migrations/20240628221915_InitialCreate.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiBarberia.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserName = table.Column<string>(type: "TEXT", nullable: false),
+                    PasswordHash = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    UserType = table.Column<int>(type: "INTEGER", nullable: false),
+                    Specialties = table.Column<int>(type: "INTEGER", nullable: true),
+                    Age = table.Column<int>(type: "INTEGER", nullable: true),
+                    PhoneNumber = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BarberShcedules",
+                columns: table => new
+                {
+                    BarberScheduleId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    BarberId = table.Column<int>(type: "INTEGER", nullable: false),
+                    CurrentYear = table.Column<int>(type: "INTEGER", nullable: false),
+                    LastModifiedDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BarberShcedules", x => x.BarberScheduleId);
+                    table.ForeignKey(
+                        name: "FK_BarberShcedules_Users_BarberId",
+                        column: x => x.BarberId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BarberAvailabilities",
+                columns: table => new
+                {
+                    BarberAvailabilityId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    BarberScheduleId = table.Column<int>(type: "INTEGER", nullable: false),
+                    DayOfTheWeek = table.Column<int>(type: "INTEGER", nullable: false),
+                    AvailabilityDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    StartTime = table.Column<TimeSpan>(type: "TEXT", nullable: false),
+                    EndTime = table.Column<TimeSpan>(type: "TEXT", nullable: false),
+                    IsAvailable = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BarberAvailabilities", x => x.BarberAvailabilityId);
+                    table.ForeignKey(
+                        name: "FK_BarberAvailabilities_BarberShcedules_BarberScheduleId",
+                        column: x => x.BarberScheduleId,
+                        principalTable: "BarberShcedules",
+                        principalColumn: "BarberScheduleId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Appointments",
+                columns: table => new
+                {
+                    AppointmentId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    DateTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    StartTime = table.Column<TimeSpan>(type: "TEXT", nullable: false),
+                    Service = table.Column<int>(type: "INTEGER", nullable: false),
+                    ClientId = table.Column<int>(type: "INTEGER", nullable: false),
+                    BarberId = table.Column<int>(type: "INTEGER", nullable: false),
+                    BarberAvailabilityId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Appointments", x => x.AppointmentId);
+                    table.ForeignKey(
+                        name: "FK_Appointments_BarberAvailabilities_BarberAvailabilityId",
+                        column: x => x.BarberAvailabilityId,
+                        principalTable: "BarberAvailabilities",
+                        principalColumn: "BarberAvailabilityId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Appointments_Users_BarberId",
+                        column: x => x.BarberId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Appointments_Users_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reviwes",
+                columns: table => new
+                {
+                    ReviewId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserName = table.Column<string>(type: "TEXT", nullable: false),
+                    Rating = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    AppointmentId = table.Column<int>(type: "INTEGER", nullable: false),
+                    UserId = table.Column<int>(type: "INTEGER", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reviwes", x => x.ReviewId);
+                    table.ForeignKey(
+                        name: "FK_Reviwes_Appointments_ReviewId",
+                        column: x => x.ReviewId,
+                        principalTable: "Appointments",
+                        principalColumn: "AppointmentId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Reviwes_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Replies",
+                columns: table => new
+                {
+                    ReplyId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ReviewId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Response = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Replies", x => x.ReplyId);
+                    table.ForeignKey(
+                        name: "FK_Replies_Reviwes_ReviewId",
+                        column: x => x.ReviewId,
+                        principalTable: "Reviwes",
+                        principalColumn: "ReviewId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_BarberAvailabilityId",
+                table: "Appointments",
+                column: "BarberAvailabilityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_BarberId",
+                table: "Appointments",
+                column: "BarberId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_ClientId",
+                table: "Appointments",
+                column: "ClientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BarberAvailabilities_BarberScheduleId",
+                table: "BarberAvailabilities",
+                column: "BarberScheduleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BarberShcedules_BarberId",
+                table: "BarberShcedules",
+                column: "BarberId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Replies_ReviewId",
+                table: "Replies",
+                column: "ReviewId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviwes_UserId",
+                table: "Reviwes",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Replies");
+
+            migrationBuilder.DropTable(
+                name: "Reviwes");
+
+            migrationBuilder.DropTable(
+                name: "Appointments");
+
+            migrationBuilder.DropTable(
+                name: "BarberAvailabilities");
+
+            migrationBuilder.DropTable(
+                name: "BarberShcedules");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
